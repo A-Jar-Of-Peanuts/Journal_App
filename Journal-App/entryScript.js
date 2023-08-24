55042af82fcdac3c893d046a9f30f796f632ff6e
A-Jar-Of-Peanuts <33380913+A-Jar-Of-Peanuts@users.noreply.github.com>
2023-08-23 19:23:29 -0700

    Add files via upload

--- Journal-App/entryScript.js
+++ Journal-App/entryScript.js
@@ -1,6 +1,17 @@
 var cur = document.getElementById("header");
 cur.innerText = "Entry " + window.localStorage.getItem("Entry");
 
+var id = "entry" + window.localStorage.getItem("Entry");
+if (window.localStorage.getItem(id) != null) {
+    document.getElementById("bodyText").value = window.localStorage.getItem(id);
+}
+
+
 document.getElementById("back").onclick = function() {
     window.location.href = "index.html";
+};
+
+document.getElementById("save").onclick = function() {
+    var text = document.getElementById("bodyText").value; 
+    window.localStorage.setItem(id, text);
 };
--- Journal-App/script.js
+++ Journal-App/script.js
@@ -1,23 +1,50 @@
 var entries = 1; 
 
+if (window.localStorage.getItem("TotalEntries") == null) {
+    window.localStorage.setItem("TotalEntries", 1); 
+    console.log("haw");
+} else {
+    entries = parseInt(window.localStorage.getItem("TotalEntries")); 
+    for(var i = 2; i<=entries; i++) {
+        var temp = i-1;
+        var oldName = ""+temp;
+        var cur = document.getElementById(oldName);
+
+        var newName = ""+i;
+        var newDiv = document.createElement("div");
+        var newText = document.createElement("p");
+        newText.innerText = "Entry "+i;
+        newDiv.setAttribute("id", newName);
+        var param = "goToJournal(" + i + ")";
+        newDiv.setAttribute("onmousedown", param);
+        newText.setAttribute("id", "body"+i);
+
+        newDiv.appendChild(newText);
+        document.body.insertBefore(newDiv, cur);
+    }
+    console.log("ye");
+}
+
 document.getElementById("addEntry").onclick = function() {
-    var oldName = "entry"+entries
+    var oldName = ""+entries
     var cur = document.getElementById(oldName);
 
     entries+=1; 
-    var newName = "entry"+entries;
+    window.localStorage.setItem("TotalEntries", entries);
+    var newName = ""+entries;
     var newDiv = document.createElement("div");
     var newText = document.createElement("p");
     newText.innerText = "Entry "+entries;
     newDiv.setAttribute("id", newName);
-    newDiv.setAttribute("onmousedown", "goToJournal()");
+    var param = "goToJournal(" + entries + ")";
+    newDiv.setAttribute("onmousedown", param);
     newText.setAttribute("id", "body"+entries);
 
     newDiv.appendChild(newText);
     document.body.insertBefore(newDiv, cur);
 };
 
-function goToJournal() {
-    window.localStorage.setItem("Entry", entries);
+function goToJournal(i) {
+    window.localStorage.setItem("Entry", i);
     window.location.href = "journalEntry.html";
 }
